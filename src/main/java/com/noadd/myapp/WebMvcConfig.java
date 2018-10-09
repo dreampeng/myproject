bcec738e4fba4b6a3eda74f4ef2c94cd85ed13e1
pengpai <815566704>
2018-10-09 17:42:41 +0800

    1

--- src/main/java/com/noadd/myapp/WebMvcConfig.java
+++ src/main/java/com/noadd/myapp/WebMvcConfig.java
@@ -22,16 +22,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
         //白名单
         List<String> excldePath = new ArrayList();
         //错误界面
-        excldePath.add("/error");
+        excldePath.add("/japi/error");
         //跳转到登陆页面
-        excldePath.add("/login");
+        excldePath.add("/japi/login");
         //获取验证码
-        excldePath.add("/validate/getcode");
+        excldePath.add("/japi/validate/getcode");
         //用户相关
-        excldePath.add("/user/isreg");
-        excldePath.add("/user/isregemail");
-        excldePath.add("/user/regist");
-        excldePath.add("/user/login");
+        excldePath.add("/japi/user/isreg");
+        excldePath.add("/japi/user/isregemail");
+        excldePath.add("/japi/user/regist");
+        excldePath.add("/japi/user/login");
 //        excldePath.add("/user/logindetail");
         //过滤所有静态文件
         excldePath.add("/**/*.*");
@@ -45,15 +45,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
         //白名单
         List<String> secretExcldePath = new ArrayList();
         //错误界面
-        secretExcldePath.add("/error");
+        secretExcldePath.add("/japi/error");
         //跳转到登陆页面
-        secretExcldePath.add("/secret/login");
+        secretExcldePath.add("/japi/secret/login");
         //过滤所有静态文件
         excldePath.add("/**/*.*");
         //黑名单
         List<String> secretAddPath = new ArrayList();
-        secretAddPath.add("/secret/**/*");
-        secretAddPath.add("/secret/**/*.html");
+        secretAddPath.add("/japi/secret/**/*");
+        secretAddPath.add("/japi/secret/**/*.html");
         registry.addInterceptor(secretInterceptor)
                 .addPathPatterns(secretAddPath)
                 .excludePathPatterns(secretExcldePath);
